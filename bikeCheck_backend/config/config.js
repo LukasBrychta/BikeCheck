0338701316cb2e7dfae74c887e3441ea09924a6d
LukasBrychta <lukybrychtak@gmail.com>
2024-11-06 16:33:26 +0100

    express server

--- bikeCheck_backend/config/config.js
+++ bikeCheck_backend/config/config.js
@@ -6,6 +6,12 @@ const config = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: process.env.DIALECT,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    }
   }
 };
--- bikeCheck_backend/server.js
+++ bikeCheck_backend/server.js
@@ -1,8 +1,21 @@
 const db = require('./models');
+const express = require('express');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 
 db.sequelize.authenticate()
     .then(() => {
-        console.log('Connection to the database has been extabilished.');
+        console.log('Connection to the database has been estabilished.');
     })
     .catch(err => {
         console.error('Unable to connect to the database: ', err);
@@ -15,3 +28,5 @@ db.sequelize.sync({ alter: true})
     .catch(err => {
         console.error('Error syncing database: ', err);
     });
+
+app.get("/")
