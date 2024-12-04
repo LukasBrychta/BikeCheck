d0ea64dc7f070817784d6b2dd82975843194da31
LukasBrychta <lukybrychtak@gmail.com>
2024-12-04 22:21:31 +0100

    components_cntroller rework

--- bikeCheck_backend/controllers/components_controller.js
+++ bikeCheck_backend/controllers/components_controller.js
@@ -1,15 +1,21 @@
 const {Component} = require('../models');
+const {Bike} = require('../models/bike');
 
 exports.getBikesComponents = async (req, res) => {
     try {
         const { bike_id } = req.params;
-        const data = await Component.findAll({
-            where: { bike_id: bike_id }
+        const data = await Bike.findByPk(bike_id, {
+            include: {
+                model: Component,
+                through: {
+                    attributes: [],
+                },
+            },
         });
 
         if (data) {
             res.send({
-                data,
+                components: data.Components,
                 message: `Components for bike with bike_id=${bike_id} retrieved successfully.`
             });
         } else {
@@ -36,19 +42,24 @@ exports.createBikesComponent = async (req, res) => {
             });
         }
 
-        const component = {
+        const bike = await Bike.findByPk(bike_id);
+        if (!bike) {
+            return res.status(404).send({ message: 'Bike not found' });
+        }
+        
+        const component = await Component.create({
             name,
             type,
             usage,
-            lifespan,
-            bike_id, // Assuming the component belongs to a bike
-        };
-
-        const data = await Component.create(component);
+            lifespan
+        });
+        
+        await bike.addComponent(component);
+    
         res.send({
-            data,
             message: `Component for bike with bike_id=${bike_id} created.`
         });
+        
     } catch (err) {
         res.status(500).send({
             message: err.message || `Error occurred while creating component.`
@@ -59,13 +70,17 @@ exports.createBikesComponent = async (req, res) => {
 exports.getBikesComponent = async (req, res) => {
     try {
         const { bike_id, component_id } = req.params;
-        const data = await Component.findOne({
-            where: { bike_id: bike_id, component_id: component_id }
+        const data = await Bike.findByPk(bike_id, {
+            include: {
+                model: Component,
+                through: {attributes: []},
+                where: {component_id: component_id}
+            },
         });
 
         if (data) {
             res.send({
-                data,
+                component: data.Components[0],
                 message: `Component with component_id=${component_id} for bike with bike_id=${bike_id} retrieved successfully.`
             });
         } else {
@@ -81,50 +96,141 @@ exports.getBikesComponent = async (req, res) => {
     }
 };
 
-exports.updateBikesComponent = async (req, res) => {
+exports.getStoredComponents = async (req, res) => {
     try {
-        const { bike_id, component_id } = req.params;
-        const [updated] = await Component.update(req.body, {
-            where: { bike_id: bike_id, component_id: component_id }
+        const { user_id } = req.params;
+
+        const components = await User.findByPk(user_id, {
+            include: {
+                model: Component,
+                through: { attributes: [] },  
+            },
         });
 
-        if (updated) {
-            res.send({
-                message: `Component with component_id=${component_id} for bike with bike_id=${bike_id} was updated.`
-            });
-        } else {
-            res.status(404).send({
-                message: `Cannot update component with component_id=${component_id} for bike with bike_id=${bike_id}.`
+        if (!components) {
+            return res.status(404).send({
+                message: `User with user_id=${user_id} not found.`,
             });
         }
+
+        res.send(components.Components);
     } catch (err) {
         res.status(500).send({
             message:
-                err.message || `Error updating component with component_id=${component_id} for bike with bike_id=${bike_id}.`
+                err.message || `Error fetching stored components for user_id=${user_id}.`,
         });
     }
 };
 
-exports.deleteBikesComponent = async (req, res) => {
+exports.createStoredComponent = async (req, res) => {
     try {
-        const { bike_id, component_id } = req.params;
-        const deleted = await Component.destroy({
-            where: { bike_id: bike_id, component_id: component_id }
+        const { user_id } = req.params;
+        const { name, type, usage, lifespan } = req.body;
+
+        if (!name || !type || !usage || !lifespan) {
+            return res.status(400).send({
+                message: "All fields (name, type, usage, lifespan) are required."
+            });
+        }
+
+        const user = await User.findByPk(user_id);
+        if (!user) {
+            return res.status(404).send({ message: 'User not found' });
+        }
+        
+        const component = await Component.create({
+            name,
+            type,
+            usage,
+            lifespan
+        });
+        
+        await user.addComponent(component);
+    
+        res.send({
+            message: `Component for user with user_id=${user_id} created.`
+        });
+        
+    } catch (err) {
+        res.status(500).send({
+            message: err.message || `Error occurred while creating component.`
+        });
+    }
+};
+
+exports.getStoredComponent = async (req, res) => {
+    try {
+        const { user_id, component_id } = req.params;
+        const data = await User.findByPk(user_id, {
+            include: {
+                model: Component,
+                through: {attributes: []},
+                where: {component_id: component_id}
+            },
         });
 
-        if (deleted) {
+        if (data) {
             res.send({
-                message: `Component with component_id=${component_id} for bike with bike_id=${bike_id} was deleted.`
+                component: data.Components[0],
+                message: `Component with component_id=${component_id} for user with user_id=${user_id} retrieved successfully.`
             });
         } else {
             res.status(404).send({
-                message: `Cannot delete component with component_id=${component_id} for bike with bike_id=${bike_id}.`
+                message: `No component found with component_id=${component_id} for bike with user_id=${user_id}.`
             });
         }
     } catch (err) {
         res.status(500).send({
             message:
-                err.message || `Error deleting component with component_id=${component_id} for bike with bike_id=${bike_id}.`
+                err.message || `Error retrieving component with component_id=${component_id} for bike with user_id=${user_id}.`
+        });
+    }
+};
+
+exports.updateComponent = async (req, res) => {
+    try {
+        const { component_id } = req.params;
+        const [updated] = await Component.update(req.body, {
+            where: { component_id: component_id }
+        });
+
+        if (updated) {
+            res.send({
+                message: `Component with component_id=${component_id} was updated.`
+            });
+        } else {
+            res.status(404).send({
+                message: `Cannot update component with component_id=${component_id}.`
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message:
+                err.message || `Error updating component with component_id=${component_id}.`
+        });
+    }
+};
+
+exports.deleteComponent = async (req, res) => {
+    try {
+        const { component_id } = req.params;
+        const deleted = await Component.destroy({
+            where: { component_id: component_id }
+        });
+
+        if (deleted) {
+            res.send({
+                message: `Component with component_id=${component_id} was deleted.`
+            });
+        } else {
+            res.status(404).send({
+                message: `Cannot delete component with component_id=${component_id}.`
+            });
+        }
+    } catch (err) {
+        res.status(500).send({
+            message:
+                err.message || `Error deleting component with component_id=${component_id}.`
         });
     }
 };
--- bikeCheck_backend/routes/components.js
+++ bikeCheck_backend/routes/components.js
@@ -5,13 +5,13 @@ const components_controller = require('../controllers/components_controller');
 router.get('/bikes/:bike_id/components', components_controller.getBikesComponents);
 router.post('/bikes/:bike_id/components', components_controller.createBikesComponent);
 router.get('/bikes/:bike_id/components/:component_id', components_controller.getBikesComponent);
-router.post('/bikes/:bike_id/components/:component_id', components_controller.updateBikesComponent);
-router.delete('/bikes/:bike_id/components/:component_id', components_controller.deleteBikesComponent);
+router.post('/bikes/:bike_id/components/:component_id', components_controller.updateComponent);
+router.delete('/bikes/:bike_id/components/:component_id', components_controller.deleteComponent);
 
 router.get('/users/:user_id/components', components_controller.getStoredComponents);
 router.post('/users/:user_id/components', components_controller.createStoredComponent);
 router.get('/users/:user_id/components/:component_id', components_controller.getStoredComponent);
-router.put('/users/:user_id/components/:component_id', components_controller.updateStoredComponent);
-router.delete('/users/:user_id/components/:component_id', components_controller.deleteStoredComponent);
+router.put('/users/:user_id/components/:component_id', components_controller.updateComponent);
+router.delete('/users/:user_id/components/:component_id', components_controller.deleteComponent);
 
 module.exports = components_controller;
