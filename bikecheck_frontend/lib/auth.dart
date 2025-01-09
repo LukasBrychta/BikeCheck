57fe5ead8dee66fb5ad7738cbcb1433c06ce9d3d
LukasBrychta <lukybrychtak@gmail.com>
2025-01-09 23:13:32 +0100

    Redirect and auth is working, token exchange remains

--- bikecheck_frontend/lib/auth.dart
+++ bikecheck_frontend/lib/auth.dart
@@ -1,19 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
-import 'dart:convert' show jsonDecode, jsonEncode;
-import 'package:go_router/go_router.dart';
-
+import 'package:bikecheck_frontend/router.config.dart';
 
 void authenticate() async {
   
   String clientId = dotenv.env['CLIENT_ID'] ?? '';
   String callbackUrl = dotenv.env['CALLBACK_URL'] ?? '';
-  String backendUrl = dotenv.env['BACKEND_URL'] ?? '';
-  print("Callback URL: $callbackUrl");
-  print("Callback URL Scheme: ${Uri.parse(callbackUrl).scheme}");
-
+  print(callbackUrl);
 
   final authUrl = Uri.https('www.strava.com', '/oauth/mobile/authorize', {
     'response_type': 'code',
@@ -25,5 +19,6 @@ void authenticate() async {
   final result = await FlutterWebAuth2.authenticate(url: authUrl.toString(), callbackUrlScheme: 'bikecheck');
 
   final code = Uri.parse(result).queryParameters['code'];
-  print("code: $code");
+  
+  router.go('/home');
 }
