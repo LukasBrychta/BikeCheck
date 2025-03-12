2f806a45e8f73e05132e31617aa950487814d056
LukasBrychta <lukybrychtak@gmail.com>
2025-03-12 22:44:09 +0100

    Showing user and bikes

--- bikecheck_frontend/lib/classes/bike.dart
+++ bikecheck_frontend/lib/classes/bike.dart
@@ -1,9 +1,19 @@
 import 'component.dart';
 
 class Bike {
-  String? bikeId;
+  String bikeId;
   String name;
+  int distance;
   int userId;
   Map<String,Component>? components;
-  Bike({this.bikeId, required this.name, required this.userId});
+  Bike({required this.bikeId, required this.name, required this.distance, required this.userId});
+
+  static Bike fromJson(data) {
+    return Bike(
+      bikeId: data['bike_id'],
+      name: data['name'],
+      distance: int.parse(data['distance']),
+      userId: int.parse(data['user_id']),
+    );
+  }
 }
--- bikecheck_frontend/lib/classes/userinfo.dart
+++ bikecheck_frontend/lib/classes/userinfo.dart
@@ -26,22 +26,19 @@ class UserInfo{
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body);
     
-    if (data.containsKey('bikes')) {
-      final List<dynamic> bikes = data['bikes'];
+    if (data.containsKey('data')) {
+      final List<dynamic> bikes = data['data'];
       print('Fetched ${bikes.length} bikes for user $id');
       
-      // Initialize bikes if null
+      // Initializing bikes map if it doesn't exist
       if (UserInfo.instance.user?.bikes == null) {
-        UserInfo.instance.user!.bikes = {}; // Ensure bikes is initialized
+        UserInfo.instance.user!.bikes = {};
       }
       
-      for (var bike in bikes) {
-        String bikeId = bike['id']; // Convert the id to int
-        String bikeName = bike['name']; // Assuming the name is a string
-        
-        // Safely update the user's bike list
-        UserInfo.instance.user!.bikes![bikeId] = Bike(bikeId: bikeId, name: bikeName, userId: id);
-        print('Bike was set with id: $bikeId, name: $bikeName and userId: $id');
+      for (var b in bikes) {
+        Bike bike = Bike.fromJson(b);
+        UserInfo.instance.user!.bikes![bike.bikeId] = bike;
+        print('Bike was set with id: ${bike.bikeId}, name: ${bike.name} and userId: $id');
         print('${UserInfo.instance.user!.bikes!.length}');
       }
     } else {
